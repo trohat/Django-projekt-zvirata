e4ae7fac2d906e1fdbe5d73bbffad0cd75dd02f7
Trohat <petr.sed@centrum.cz>
2021-10-15 11:16:01 +0200

    Admin for 'Zvire'

--- informace/admin.py
+++ informace/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Zvire
 
 # Register your models here.
+
+class ZvireAdmin(admin.ModelAdmin):
+    list_display = ("id", "jmeno", "vaha", "barva", "zije")
+    list_filter = ("jmeno", "zije")
+
+admin.site.register(Zvire, ZvireAdmin)
